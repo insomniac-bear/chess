e810474ad6b769dab73269c3188d000d1cf7f24c
Cherepanov Kirill <tcherepanov.kir@gmail.com>
2023-01-25 10:57:03 +0300

    first commit

---
+++ src/components/app/app.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import styles from './app.module.css';
+
+export const App: FC = () => {
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+}
---
+++ src/components/board/board.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react';
+import styles from './board.module.css';
+
+export const Board: FC = () => {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  );
+};
