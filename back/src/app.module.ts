6e60bc991211fe930a33dcfe838df6294d3f403e
Cherepanov Kirill <tcherepanov.kir@gmail.com>
2023-04-16 00:45:06 +0300

    feat: #14 create connector for database

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseService } from './database/database.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseService,
+      inject: [DatabaseService],
+    }),
+  ],
+  controllers: [],
+  providers: [DatabaseService],
 })
 export class AppModule {}
