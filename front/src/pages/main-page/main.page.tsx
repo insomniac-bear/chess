57138664fa2becfd090dcb7c7601038650308767
Cherepanov Kirill <tcherepanov.kir@gmail.com>
2023-06-24 20:56:37 +0300

    [CHESS-22] add header for main page

---
+++ front/src/pages/main-page/main.page.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+import { Header } from '../../components/header/header';
+
+export const MainPage: FC = () => {
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
+};
